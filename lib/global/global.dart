b6a6c5665dcd18bb37df709e711f3a461c624912
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-22 11:24:52 +0545

    Update global.dart

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
 class Global {
   static String author = "";
   static String book = "";
+  static String encodedImage = "";
+  static Uint8List? image;
+  static Uint8List? decodedImage;
 }
